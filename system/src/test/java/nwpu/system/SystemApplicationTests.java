98c394d010f30a508ffbcb0791d8d97bbe67ae7b
yuyu <3535826954@qq.com>
2024-06-03 13:18:11 +0800

    try

---
+++ system/src/test/java/nwpu/system/SystemApplicationTests.java
@@ -0,0 +1,13 @@
+package nwpu.system;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SystemApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
