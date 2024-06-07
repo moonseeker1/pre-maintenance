9e093460e7967f1cd8258579a36f58d60def44ed
wugouguayue <2290166829@qq.com>
2024-06-07 21:09:11 +0800

    维修人员端

--- repair_person/src/main/java/com/system/repair_person/mapper/EquipmentMapper.java
+++ repair_person/src/main/java/com/system/repair_person/mapper/EquipmentMapper.java
@@ -2,6 +2,7 @@ package com.system.repair_person.mapper;
 
 import com.system.repair_person.model.Equipment;
 import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import org.apache.ibatis.annotations.Mapper;
 
 /**
  * <p>
@@ -11,6 +12,7 @@ import com.baomidou.mybatisplus.core.mapper.BaseMapper;
  * @author wugou
  * @since 2024-06-07
  */
+@Mapper
 public interface EquipmentMapper extends BaseMapper<Equipment> {
 
 }
