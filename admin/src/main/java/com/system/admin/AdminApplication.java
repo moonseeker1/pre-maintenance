1a1930fee1671232343345b34764a89a5966ccea
wugouguayue <2290166829@qq.com>
2024-06-07 16:36:47 +0800

    订单详情和分页查询

--- admin/src/main/java/com/system/admin/AdminApplication.java
+++ admin/src/main/java/com/system/admin/AdminApplication.java
@@ -2,9 +2,11 @@ package com.system.admin;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 @EnableTransactionManagement
+@EnableScheduling
 @SpringBootApplication(scanBasePackages = {"com.system.admin", "com.system.common", "com.system.security"})
 public class AdminApplication {
 
